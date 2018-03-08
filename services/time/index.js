3586a7fee5b1f85ee242172979f511433e5f2c95
Tucker Osman <osmiumusa@gmail.com>
2018-03-08 13:59:38 -0500

    Added support for local time

--- services/time/index.js
+++ services/time/index.js
@@ -7,6 +7,10 @@ module.exports = {
     query: (params, config, say) => {
         console.log(params);
         const where = params["geo-country"].stringValue || params["geo-city"].stringValue;
+        if(where == "") {
+            say(moment().format(`[The time is] h m A`));
+            return;
+        }
         console.log(where);
         request.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${where}&key=${config.gapikey}`, (error, response, body) => {
             if(error) {
@@ -20,8 +24,8 @@ module.exports = {
             request.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Date.now()/1000}&key=${config.gapikey}`, (error, response, body) => {
                 let data2 = JSON.parse(body);
                 const time = moment.tz(data2.timeZoneId);
-                if(time.format('e') == moment().format('e')) say(time.format(`[The time in ${where} is] h m a`));
-                else say(time.format(`[The time in ${where} is] h m a [on] dddd`));
+                if(time.format('e') == moment().format('e')) say(time.format(`[The time in ${where} is] h m A`));
+                else say(time.format(`[The time in ${where} is] h m A [on] dddd`));
             });
         });
     }
